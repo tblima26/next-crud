64a04ad3643bb87c346c752354c21ad559c24b46
Thiago Borges <tblima26@gmail.com>
2025-04-21 17:55:17 -0300

    Partial Frontend Upload
    Uploading initial frontend structure and layout components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,75 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Search, PlusCircle } from 'lucide-react';
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-7xl text-center p-8 bg bg-cyan-600 text-white">Hello World!</h1>
-    </>
+    <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <h1 className="text-4xl font-bold">Produtos</h1>
+      <div className="flex items-center justify-between">
+        <form className="flex items-center gap-2">
+          <Input name="id" placeholder="Id do Pedido" className="w-auto" />
+          <Input name="name" placeholder="Nome do Pedido" className="w-auto" />
+          <Button type="submit" variant="link">
+            <Search className="h-4 w-auto mr-2" />
+            Filtrar
+          </Button>
+        </form>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <PlusCircle className="h-4 w-auto mr-2" />
+              Novo Produto
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Produto</DialogTitle>
+              <DialogDescription>Tela para novo produto</DialogDescription>
+            </DialogHeader>
+            <form className="space-y-6">
+              <div className=" grid grid-cols-6 items-center text-right gap-3">
+                <Label htmlFor="name">Produto</Label>
+                <Input className="col-span-3" id="name" />
+              </div>
+              <div className=" grid grid-cols-6 items-center text-right gap-3">
+                <Label htmlFor="price">Preço</Label>
+                <Input className="col-span-3" id="price" />
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button type="button" variant="outline"> Cancelar </Button>
+                </DialogClose>
+                <Button type="submit" > Salvar </Button>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="border rounded-lg p-2">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Produto</TableHead>
+              <TableHead>Preço</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell>{i}</TableCell>
+                <TableCell>Produto {i}</TableCell>
+                <TableCell>R$ 192,00</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 }
