ec50a4f19b56e392a74943f67185942c59a05178
Thiago Borges <tblima26@gmail.com>
2025-04-21 16:34:38 -0300

    Tailwind installed and running.
    Tailwind installed and running without errors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-7xl text-center p-8 bg bg-cyan-600 text-white">Hello World!</h1>
     </>
   );
 }
